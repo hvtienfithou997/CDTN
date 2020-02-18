46ca34d92c314db80df7f2389fd03607e4b8b719
nghiep.hathe <nghiep.hathe@euroland.com>
2020-02-18 11:40:35 +0700

    Tham trang chu

--- FoodCleanB/Controllers/SanPhamController.cs
+++ FoodCleanB/Controllers/SanPhamController.cs
@@ -1,12 +1,35 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using FoodCleanB.Database;
 
 namespace FoodCleanB.Controllers
 {
-    public class SanPhamController : Controller
+    public class SanPhamController : BaseController
     {
-        public ActionResult Detail(int itemid)
+        public PartialViewResult HomeProduct(int? nhomHang)
         {
-            throw new System.NotImplementedException();
+            IQueryable<SanPham> list;
+            if (nhomHang != null)
+            {
+                 list =  Db.SanPhams.Where(o => o.MaNhomHang == nhomHang);
+
+            }
+            else
+            {
+                list = Db.SanPhams.Take(10);
+            }
+
+            return PartialView(list);
+        }
+
+        [Route("san-pham/{itemId}-{title}")]
+        [HttpGet]
+        public ActionResult Detail(int itemId)
+        {
+            var sanPham = Db.SanPhams.FirstOrDefault(o => o.MaHang == itemId);
+
+            return View(sanPham);
         }
     }
 }
